dc5e36ab2d685ce468f7970a52724ed269700c17
Zeelvegda <140957945+Zeelvegda@users.noreply.github.com>
2024-03-15 16:54:54 +0530

    Add files via upload

---
+++ lab1-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	printf("enter a ");
+	scanf("%d",&a);
+	printf("enter b ");
+	scanf("%d",&b);
+	printf("enter c ");
+	scanf("%d",&c);
+	d=(a+b+c)/3;
+	printf("average=%d",d);
+}
