ea8d39e6f826b72696dbf75db73982c89ac4f954
Zeelvegda <140957945+Zeelvegda@users.noreply.github.com>
2024-03-17 18:29:18 +0530

    Add files via upload

---
+++ lab1-4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	float h,b,area;
+	printf("enter h ");
+	scanf("%f",&h);
+    printf("enter b ");
+	scanf("%f",&b);
+	area=(h*b)/2;
+	printf("area of triangle=%f",area);
+}
