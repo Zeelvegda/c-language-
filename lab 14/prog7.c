9a35f3eb6791f8eedd39808b50752b52a58dba27
Zeelvegda <140957945+Zeelvegda@users.noreply.github.com>
2024-04-11 17:43:24 +0530

    Add files via upload

---
+++ lab 14/prog7.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+void main()
+{
+    int arr[5],i;
+    for(i=0;i<5;i++)
+    {
+        printf("enter a value[%d]: ",i);
+        scanf("%d",&arr[i]);
+    }
+        for(i=4;i>=0;i--)
+    {
+        printf("arr[%d]: %d\n",i,arr[i]);
+    }
+}
+
