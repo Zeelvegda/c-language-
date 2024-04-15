c3fe558e8805345a65ecefc1399042a4613f3bd5
Zeelvegda <140957945+Zeelvegda@users.noreply.github.com>
2024-04-15 18:15:31 +0530

    Add files via upload

---
+++ lab 17/prog1.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+void main()
+{
+    int number = 29; 
+    int *ptr = &number; 
+    printf("Value of the variable: %d\n", *ptr);
+    printf("Address of the variable: %d", ptr);
+}
+
