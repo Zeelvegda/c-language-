7ebb3eadcc702cb9ca9c4140e08bc783bc3a58e3
Zeelvegda <140957945+Zeelvegda@users.noreply.github.com>
2024-04-25 13:45:46 +0530

    Add files via upload

---
+++ lab 18/prog 6.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int a,b;
+int add(int a,int b);
+void main()
+{
+    scanf("%d %d",&a,&b);
+    int res=add(a,b);
+    printf("%d",res);
+}
+int add(int a,int b)
+{
+    int c=a+b;
+    return c;
+}
+
